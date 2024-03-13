7d53958c24d8b499e9c146b0811fc8b9973d8db9
mr4dd <158076664+mr4dd@users.noreply.github.com>
2024-03-13 18:23:31 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# todew
+An easy to use and pretty minimal command line utility for managing tasks, written entirely in python.
+
+#   Installation
+
+1. clone the repo:
+```sh
+git clone https://github.com/mr4dd/todew.git
+```
+-------------
+2. cd into the directory:
+```sh 
+cd todew/src/
+```
+-------------
+3. run 
+```sh 
+sh setup.sh
+```
+-------------
---
+++ src/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source_file="todew"
+install_dir="/usr/local/bin"
+executable_name="todew"
+
+# Copy the Python file to the installation directory
+sudo cp "$source_file" "$install_dir"
+
+# Make the file executable
+sudo chmod +x "$install_dir/$source_file"
+
+# Create a symbolic link to make it callable from anywhere
+sudo ln -s "$install_dir/$source_file" "$install_dir/$executable_name"
+
+echo "Installation complete. You can now call '$executable_name' from the terminal."
+
